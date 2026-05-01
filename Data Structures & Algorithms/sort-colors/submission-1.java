beb3d2f069c271b09461611f882b87decec2b3d2
smk-rifat <144414559+smk-rifat@users.noreply.github.com>
2026-05-02 00:18:51 +0600

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int zero = 0, one = 0, two = 0;
+
+        for(int num : nums){
+            if(num == 0){
+                zero++;
+            } else if(num == 1){
+                one++;
+            } else {
+                two++;
+            }
+        }
+        int idx = 0;
+        while(zero > 0) {nums[idx++] = 0; zero--;} 
+        while(one > 0) {nums[idx++] = 1; one--;} 
+        while(two > 0) {nums[idx++] = 2; two--;} 
+    }
+}
