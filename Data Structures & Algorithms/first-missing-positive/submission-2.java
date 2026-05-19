c65b85b145d6c96813675a1959e1b218ef632b9c
smk-rifat <144414559+smk-rifat@users.noreply.github.com>
2026-05-19 23:46:28 +0600

    Add: first-missing-positive - submission-2

---
+++ Data Structures & Algorithms/first-missing-positive/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        //boolean[] seen = new boolean[n + 1];
+
+        for(int i = 0; i < n; i++){
+            while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]){
+                swap(nums, nums[i] - 1, i);
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] != i + 1) return i + 1;
+        }
+
+        return n + 1;
+    }
+    void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
